4f7af28b8732438a3459a6a4370738b2f8fda971
Anatoly Ch <googlsocial97@gmail.com>
2022-04-28 03:44:16 +0700

    Initial Bot.UniversalLibrary.Infrastructure
    Add  Bot.UniversalLibrary.Infrastructure.
    Add IClient and update Bot.

--- Bot.UniversalLibrary.Core/Bot.cs
+++ Bot.UniversalLibrary.Core/Bot.cs
@@ -24,9 +24,33 @@ public enum Status
 public abstract class Bot : IBot
 {
     /// <summary>
+    ///     Initializes a new instance of the <see cref="Bot" /> class.
+    /// </summary>
+    protected Bot()
+    {
+        BotState = Status.Off;
+        Clients = new List<IClient>();
+    }
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="Bot" /> class.
+    /// </summary>
+    /// <param name="clients">List of bot clients (example: telegram, discord).</param>
+    protected Bot(IList<IClient> clients)
+    {
+        BotState = Status.Off;
+        Clients = clients ?? throw new ArgumentNullException(nameof(clients));
+    }
+
+    /// <summary>
     ///     Gets or sets the current state of the bot.
     /// </summary>
-    public Status BotState { get; protected set; } = Status.Off;
+    public Status BotState { get; protected set; }
+
+    /// <summary>
+    ///     Gets or sets a list of clients that are responsible for interacting with the target platform.
+    /// </summary>
+    public IList<IClient> Clients { get; protected set; }
 
     /// <summary>
     ///     Bot launch.
@@ -34,6 +58,7 @@ public abstract class Bot : IBot
     public void Start()
     {
         BotState = Status.On;
+        foreach (var client in Clients) client.Start();
     }
 
     /// <summary>
@@ -42,5 +67,6 @@ public abstract class Bot : IBot
     public void Stop()
     {
         BotState = Status.Off;
+        foreach (var client in Clients) client.Stop();
     }
 }
---
+++ Bot.UniversalLibrary.Core/Interfaces/IClient.cs
@@ -0,0 +1,8 @@
+﻿namespace Bot.UniversalLibrary.Core.Interfaces;
+
+/// <summary>
+///     Bot client, responsible for interacting with the end platform.
+/// </summary>
+public interface IClient : IStartable, IStopable
+{
+}
